80cf3a45756c0aa027e2155b2f00a3f31e057ee7
Charlie Hothersall-Thomas <me@charlie.ht>
2021-12-16 17:22:47 +0000

    Add day 16 part two

--- day16/day16.go
+++ day16/day16.go
@@ -1,10 +1,29 @@
 package day16
 
-func Part1(bs BitString) (int, error) {
+func Part1(in string) (int, error) {
+	bs, err := NewBitString(in)
+	if err != nil {
+		return 0, err
+	}
+
 	p, err := bs.ReadPacket()
 	if err != nil {
 		return 0, err
 	}
 
 	return p.TotalVersion(), nil
+}
+
+func Part2(in string) (int, error) {
+	bs, err := NewBitString(in)
+	if err != nil {
+		return 0, err
+	}
+
+	p, err := bs.ReadPacket()
+	if err != nil {
+		return 0, err
+	}
+
+	return p.Value()
 }
--- day16/packet.go
+++ day16/packet.go
@@ -1,9 +1,22 @@
 package day16
 
+import (
+	"fmt"
+
+	"github.com/chigley/advent2021"
+)
+
 type TypeID int
 
 const (
-	Literal TypeID = 4
+	Sum TypeID = iota
+	Product
+	Min
+	Max
+	Literal
+	Gt
+	Lt
+	Eq
 )
 
 type Packet struct {
@@ -22,4 +35,50 @@ func (p *Packet) TotalVersion() int {
 		}
 	}
 	return sum
+}
+
+func (p *Packet) Value() (int, error) {
+	if p.Type == Literal {
+		return p.LiteralValue, nil
+	}
+
+	subPacketValues := make([]int, len(p.SubPackets))
+	for i, pp := range p.SubPackets {
+		var err error
+		subPacketValues[i], err = pp.Value()
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	switch p.Type {
+	case Sum:
+		return advent2021.Sum(subPacketValues), nil
+	case Product:
+		return advent2021.Product(subPacketValues), nil
+	case Min:
+		return advent2021.MinInts(subPacketValues), nil
+	case Max:
+		return advent2021.MaxInts(subPacketValues), nil
+	case Gt:
+		if subPacketValues[0] > subPacketValues[1] {
+			return 1, nil
+		} else {
+			return 0, nil
+		}
+	case Lt:
+		if subPacketValues[0] < subPacketValues[1] {
+			return 1, nil
+		} else {
+			return 0, nil
+		}
+	case Eq:
+		if subPacketValues[0] == subPacketValues[1] {
+			return 1, nil
+		} else {
+			return 0, nil
+		}
+	default:
+		return 0, fmt.Errorf("day16: invalid packet type %d", p.Type)
+	}
 }
